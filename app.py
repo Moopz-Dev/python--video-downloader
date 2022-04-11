2bd62ede658b85846505aaf5cb1d4c9d85ee14c3
Moopz-Dev <pongzthor@gmail.com>
2022-04-11 10:00:08 +0700

    ✨ feat(app.py): implements download
    implements the youtube video downloading feature using pytube library

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from tkinter import *
+from pytube import YouTube
+from moviepy.editor import *
 
 root = Tk()
 root.title("Video Downloader")
 canvas = Canvas(root, width=400, height=200)
 canvas.pack()
+
+# commands
+
+
+def download():
+    video_path = link_entry.get()
+    mp4 = YouTube(video_path).streams.get_highest_resolution().download()
+    video_clip = VideoFileClip(mp4)
+    video_clip.close()
+
+
 # GUI widgets
 app_title = Label(root, text="Download Videos",
                   font=("Arial", 20, 'bold'))
@@ -15,7 +28,7 @@ canvas.create_window(200, 80, window=link_label)
 link_entry = Entry(root, width=60)
 canvas.create_window(200, 100, window=link_entry)
 
-download_button = Button(text="Download")
+download_button = Button(text="Download", command=download)
 canvas.create_window(200, 150, window=download_button)
 
 root.mainloop()
